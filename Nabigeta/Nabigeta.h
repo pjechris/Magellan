217f0812570537b33484f3f88adefe8f5ca050f2
pjechris <jcpastant@xebia.fr>
2014-11-04 18:19:25 +0100

    project setup

---
+++ Nabigeta/Nabigeta.h
@@ -0,0 +1,19 @@
+//
+//  Nabigeta.h
+//  Nabigeta
+//
+//  Created by JC on 04/11/14.
+//  Copyright (c) 2014 fr.milkshake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nabigeta.
+FOUNDATION_EXPORT double NabigetaVersionNumber;
+
+//! Project version string for Nabigeta.
+FOUNDATION_EXPORT const unsigned char NabigetaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nabigeta/PublicHeader.h>
+
+
