a948025d107417fb6f3682fe01f704b5f10b2698
pjechris <jcpastant@xebia.fr>
2014-12-01 17:09:34 +0100

    [route] added SegueRoute

---
+++ Nabigeta/Route/SegueRoute.swift
@@ -0,0 +1,25 @@
+//
+//  SegueRoute.swift
+//  Nabigeta
+//
+//  Created by JC on 01/12/14.
+//  Copyright (c) 2014 fr.milkshake. All rights reserved.
+//
+
+import Foundation
+
+public class SegueRoute : Routable {
+    public let context: String
+    public let segueIdentifier: String
+
+    lazy public var navigationStrategy: NavigationStrategy = SegueStrategy(route: self)
+
+    public init(context: String, segueIdentifier: String) {
+        self.context = context
+        self.segueIdentifier = segueIdentifier
+    }
+
+    public func match(context: String) -> Bool {
+        return context == context
+    }
+}
