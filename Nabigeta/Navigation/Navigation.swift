f22a3a5f0213fba589a3c57ad2dd3628894253f8
pjechris <jcpastant@xebia.fr>
2015-10-28 11:40:59 +0100

    [navigation] Navigator -> Navigation

--- Nabigeta/Navigation/Navigation.swift
+++ Nabigeta/Navigation/Navigation.swift
@@ -1,15 +1,15 @@
 //
-//  RouteCollection.swift
-//  Nabigeta
+// This file is part of Nabigeta
 //
-//  Created by JC on 04/11/14.
-//  Copyright (c) 2014 fr.milkshake. All rights reserved.
+// Created by JC on 04/11/14.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
 //
 
 import Foundation
 import UIKit
 
-public class Navigator : NSObject {
+public class Navigation : NSObject {
     public var willNavigate: PresentationWillShowHandler?
 
     private var routesCollection: Array<RouteCollection>
