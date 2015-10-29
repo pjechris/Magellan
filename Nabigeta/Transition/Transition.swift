b54fa9141a71deab53335e559aa9e50514939366
pjechris <jcpastant@xebia.fr>
2015-10-29 17:24:04 +0100

    [transition] added transition models

---
+++ Nabigeta/Transition/Transition.swift
@@ -0,0 +1,14 @@
+//
+// This file is part of Nabigeta
+//
+// Created by JC on 29/10/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+import Foundation
+
+internal struct Transition {
+    let presentation: PresentationStyle
+    let trait: UITraitCollection?
+}
---
+++ Nabigeta/Transition/TransitionContext.swift
@@ -0,0 +1,28 @@
+//
+// This file is part of Nabigeta
+//
+// Created by JC on 29/10/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+import Foundation
+
+internal struct TransitionContext : Hashable {
+    let fromViewController: UIViewController.Type?
+    let toViewController: UIViewController.Type
+
+    var hashValue: Int {
+        get { return self.toViewController.hash() ^ (fromViewController?.hash() ?? 1) }
+    }
+
+    init(from: UIViewController.Type?, to: UIViewController.Type) {
+        self.fromViewController = from
+        self.toViewController = to
+    }
+}
+
+func ==(lhs: TransitionContext, rhs: TransitionContext) -> Bool {
+    return lhs.toViewController == rhs.toViewController && lhs.fromViewController == rhs.fromViewController
+}
+
