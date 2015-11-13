e6d8cf285f7af517cba29ab47efc97f8ff85cd4e
pjechris <jcpastant@xebia.fr>
2015-11-13 14:49:18 +0100

    [presentation] added popover presentation

---
+++ Nabigeta/Presentation/PresentationPopover.swift
@@ -0,0 +1,31 @@
+//
+// This file is part of Nabigeta
+//
+// Created by JC on 12/11/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+import Foundation
+import UIKit
+
+public class PresentationPopover : PresentationStrategy {
+
+    public func show(navigationContext: NavigationContext, willShow: PresentationWillShowHandler?) {
+        let destinationController = navigationContext.route.destination.init()
+        let stackController: UINavigationController = UINavigationController()
+
+        willShow?(destinationController, navigationContext.context)
+
+        stackController.modalPresentationStyle = .Popover
+        stackController.pushViewController(destinationController, animated: false)
+        navigationContext.sourceViewController.presentViewController(stackController, animated: true, completion: nil)
+
+        stackController.popoverPresentationController!.sourceView = navigationContext.touchedControl
+        stackController.popoverPresentationController!.sourceRect = navigationContext.touchedControl!.bounds
+    }
+
+    public func dismiss(sender: UIViewController) {
+        sender.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
