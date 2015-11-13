a571ff3f60feff96a89cc99318136fbc7a7e133b
pjechris <jcpastant@xebia.fr>
2015-11-13 16:13:06 +0100

    [lib] removed PresentationStyle class

--- Nabigeta/Navigation/NavigationCollection.swift
+++ Nabigeta/Navigation/NavigationCollection.swift
@@ -1,9 +1,9 @@
 //
-//  NavigationCollection.swift
-//  Nabigeta
+// This file is part of Nabigeta
 //
-//  Created by JC on 29/10/15.
-//  Copyright © 2015 fr.milkshake. All rights reserved.
+// Created by JC on 29/10/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
 //
 
 import Foundation
