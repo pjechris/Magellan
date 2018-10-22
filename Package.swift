72a74b70ccddf5fb7af24fbbc5b5737a5077ccdd
pjechris <jcpastant@xebia.fr>
2018-10-22 17:18:50 +0200

    [package] Added Quick for testing + re-generated xcodeproj with swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Magellan"
+    name: "Magellan",
+    products: [
+        .library(
+            name: "Magellan",
+            targets: ["Magellan"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/quick", from: "1.3.2")
+    ],
+    targets: [
+        .target(
+            name: "Magellan",
+            dependencies: []),
+        .testTarget(
+            name: "MagellanTests",
+            dependencies: ["Magellan", "Quick"],
+            path: "Tests"),
+    ]
 )
